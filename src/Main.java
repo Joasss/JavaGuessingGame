75d15ad97067d9e29cce70df160f97cafa324c28
Joa_sss <quabotgithub@gmail.com>
2022-08-10 22:43:15 +0200

    Rewrote using while loop

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,44 @@
 import java.util.Scanner;
 
-public class Main {
-    static int userInput;
-    static int computerNum = (int) (Math.random() * 11);
+class Main {
+    int userGuess = 0;
 
-    static void getInput() {
-        Scanner numInput = new Scanner(System.in);
-        System.out.println("Enter a number between 1 and 10.");
+    public void getInput() {
+        System.out.println("Please enter a number between 1-100 to guess.");
+        Scanner inputUser = new Scanner(System.in);
         try {
-            userInput = numInput.nextInt();
-            checkResults();
+            userGuess = inputUser.nextInt();
         } catch (Exception e) {
-            System.out.println("Please enter a valid number.");
-            getInput();
-        }
-    }
-    static void checkResults() {
-        if (userInput < 0 || userInput > 10) {
-            System.out.println("Please enter a valid number between 1 and 10.");
-            getInput();
-        }
-
-        if (userInput == computerNum) {
-            System.out.println("You guessed the number!");
-            System.out.println(" ");
-            computerNum = (int) (Math.random() * 11);
-            getInput();
-        } else if (userInput < computerNum) {
-            System.out.println("The number is higher!");
-            getInput();
-        } else {
-            System.out.println("The number is lower.");
             getInput();
         }
     }
 
-    public static void main(String[] args) { getInput(); }
+    public static void main(String[] args) {
+        Main obj = new Main();
+        int computerNumber = (int) (Math.random() * 100 + 1);
+        int guessCount = 0;
 
+        obj.getInput();
+
+        while (obj.userGuess != computerNumber) {
+            if (obj.userGuess < 1 || obj.userGuess > 101) {
+                obj.getInput();
+                continue;
+            }
+
+            guessCount++;
+
+            if (computerNumber > obj.userGuess) {
+                System.out.println("The number is higher!");
+                obj.getInput();
+            } else {
+                System.out.println("The number is lower!");
+                obj.getInput();
+            }
+        }
+
+        guessCount++;
+        System.out.println("You were correct!");
+        System.out.println("You got the answer in " + guessCount + " guesses!");
+    }
 }
